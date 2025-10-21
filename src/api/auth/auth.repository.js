00df0aaacb9a3f92287553e1245a3c3cea560e75
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-10-21 12:10:10 +0900

    auth 기본 기능
    - 회원가입
    - 로그인
    - 소셜 로그인
    - 인가 여부 미들웨어
    - 인가 여부 엔드포인트
    - strategy
    - utils (token, hash)
    - 라우터 연결 (app -> api -> auth)
    
    - prisma 유저, 테스트 테이블 스키마 추가(로컬 마이그레이션)
    
    - 기능 별 정리
    	- auth.controller
    	- auth.repository
    	- auth.router
    	- auth.service

--- src/api/auth/auth.repository.js
+++ src/api/auth/auth.repository.js
@@ -1,2 +1,57 @@
-// 예시파일입니다. 필요시 지우고 사용하세요.
-// import prisma from "../../config/db.js";
+//auth repo고 auth에서만 사용하긴 하지만,
+//user 테이블에 관한 내용 밖에 없네요..
+
+import { prisma } from "../../config/db.js";
+
+//유저 테이블 CRUD 함수들.
+
+async function findById(id) {
+  return prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+async function findByEmail(email) {
+  return await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+}
+
+async function save(user) {
+  return prisma.user.create({
+    data: {
+      email: user.email,
+      name: user.name,
+      password: user.password,
+    },
+  });
+}
+
+async function update(id, data) {
+  return prisma.user.update({
+    where: {
+      id,
+    },
+    data: data,
+  });
+}
+
+async function createOrUpdate(provider, providerId, email, name) {
+  return prisma.user.upsert({
+    where: { provider, providerId },
+    update: { email, name },
+    create: { provider, providerId, email, name },
+  });
+}
+
+export default {
+  findById,
+  findByEmail,
+  save,
+  update,
+  createOrUpdate,
+};
