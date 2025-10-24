d53aafbb03c4582d12cb8d35cb95891c745f7dc6
DESKTOP-14A5R6M\clwns <clwns00@naver.com>
2025-10-24 11:09:27 +0900

    fix: 구매자 상세 API 유저 기능 추가된 버전으로 수정 (로그인 시 동작 가능)

--- src/api/products/products.routes.js
+++ src/api/products/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-// import { authGuard } from "../../middlewares/authGuard.js";
+import { verifyAccessToken } from "../../middlewares/authGuard.js";
 import { validate } from "../../middlewares/validate.middleware.js";
 import {
   purchaseSchema,
@@ -12,17 +12,17 @@ const router = Router();
 
 router.post(
   "/marketplace/:listingId/purchase",
-  //   authGuard,           //authGuard는 나중에 적용
+  verifyAccessToken,
   validate(purchaseSchema),
-  controller.purchase
+  controller.purchase,
 );
 
 // 교환 신청 생성
 router.post(
   "/marketplace/:listingId/exchanges",
-  //   authGuard, //authGuard는 나중에 적용
+  verifyAccessToken,
   validate(createExchangeSchema),
-  controller.createExchangeOffer
+  controller.createExchangeOffer,
 );
 
 // 마켓플레이스 판매 카드 목록 조회
@@ -32,16 +32,9 @@ router.get("/marketplace", controller.getMarketplaceListings);
 router.get("/marketplace/my-photo-cards", controller.getMyPhotoCards);
 
 // 포토카드 상세 조회
-router.get(
-  "/marketplace/my-photo-cards/:myPhotoCardId",
-  controller.getMyPhotoCardById
-);
+router.get("/marketplace/my-photo-cards/:myPhotoCardId", controller.getMyPhotoCardById);
 
 // 판매 등록
-router.post(
-  "/marketplace/listings",
-  validate(createListingSchema),
-  controller.createListing
-);
+router.post("/marketplace/listings", validate(createListingSchema), controller.createListing);
 
 export default router;
