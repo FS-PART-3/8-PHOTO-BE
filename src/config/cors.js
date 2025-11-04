39ec9cfdde9f7dc0e509e683bb9d9f06c08077c5
chaewon127 <osobi621@gmail.com>
2025-11-04 14:24:55 +0900

    fix: cors 설정 원복

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,13 +1,27 @@
 import cors from "cors";
 import { env } from "./env.js";
 
-const allowedOrigins = env.CORS_ORIGIN
-  ? env.CORS_ORIGIN.split(",").map((origin) => origin.trim())
-  : [];
+// const allowedOrigins = env.CORS_ORIGIN
+//   ? env.CORS_ORIGIN.split(",").map((origin) => origin.trim())
+//   : [];
+
+// export const corsMiddleware = cors({
+//   origin: (origin, cb) => {
+//     if (!origin || env.CORS_ORIGIN === "*" || allowedOrigins.includes(origin)) {
+//       return cb(null, true);
+//     }
+//     cb(new Error("Not allowed by CORS"));
+//   },
+//   credentials: true,
+// });
 
 export const corsMiddleware = cors({
   origin: (origin, cb) => {
-    if (!origin || env.CORS_ORIGIN === "*" || allowedOrigins.includes(origin)) {
+    if (
+      !origin ||
+      env.CORS_ORIGIN === "*" ||
+      origin.includes(env.CORS_ORIGIN)
+    ) {
       return cb(null, true);
     }
     cb(new Error("Not allowed by CORS"));
