6fe061e2affeb38c588bda86f68ef27d83361350
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-11-06 19:28:45 +0900

    feat/포인트 기록 무한 스크롤

--- src/api/points/points.controller.js
+++ src/api/points/points.controller.js
@@ -8,9 +8,7 @@ import {
 export async function pointHistory(req, res, next) {
   try {
     const userId = req.auth?.userId;
-    const user = await authRepository.findById(userId);
-
-    const result = await getPointHistory(user);
+    const result = await getPointHistory(userId);
     if (!result) {
       const error = new Error("해당 유저가 없거나, 유저의 포인트 정보 없음");
       error.code = 400;
--- src/api/points/points.repository.js
+++ src/api/points/points.repository.js
@@ -1,10 +1,39 @@
 import { v4 as uuidv4 } from "uuid";
 import { prisma } from "../../config/db.js";
 
-export async function getPointHistory(user) {
+export async function getPointHistory(userId) {
   const pointHistory = prisma.point.findMany({
-    where: { userId: user.id },
+    where: { userId },
+    orderBy: { createdAt: "desc" },
   });
+  return pointHistory;
+}
+
+// 내 포토카드 목록 조회 +검색/필터/정렬
+export async function getPointHistoryListing({
+  userId,
+  cursor,
+  search = "",
+  sort = "latest",
+  take = 10,
+}) {
+  const where = {
+    userId,
+    ...(search && { createdAt: { contains: search, mode: "insensitive" } }),
+  };
+
+  const orderBy =
+    sort === "latest" ? { createdAt: "desc" } : { createdAt: "asc" };
+
+  const pointHistory = prisma.point.findMany({
+    where,
+    include: { user: { select: { name: true, id: true } } },
+    orderBy,
+    take,
+    skip: cursor ? 1 : 0,
+    ...(cursor && { cursor: { id: cursor } }),
+  });
+
   return pointHistory;
 }
 
